10047e3c4245e260eb7721f2989c4da83992dc96
Trygve Lie <post@trygve-lie.com>
2018-11-22 10:02:12 +0100

    Doc and linting

--- README.md
+++ README.md
@@ -1,3 +1,119 @@
 # objobj
 
-Get and set properties in a deeply nested object structure
+Get and set properties in deeply nested object structures.
+
+## Installation
+
+```bash
+$ npm i objobj
+```
+
+## Usage
+
+Set and get a value in a deeply neste object structure:
+
+```js
+const objobj = require('objobj');
+
+const obj = objobj.get('a.b.c', 'cheeze');
+const val = objobj.get('a.b.c', obj);
+
+console.log(obj); // { a: { b: { c: 'cheeze' } } }
+console.log(val); // cheeze
+```
+
+## API
+
+ObjObj has the following methods:
+
+### .get(path, object)
+
+Get a value from a deeply nested object structure.
+
+```js
+const objobj = require('objobj');
+
+const obj = {
+    a: {
+        b: {
+            c: 'cheeze',
+        }
+    }
+}
+
+const val = objobj.get('a.b.c', obj);
+console.log(val) // cheeze
+```
+
+This method take the following arguments:
+
+ * **path** - `String` - The path, in dot notation, of the property to get - Required
+ * **object** - `Object` - The object to query - Required
+
+If the `path` does not match anything in the `object` the method will return
+`undefined`.
+
+
+### .set(path, value, object)
+
+Set a value into a deeply nested object structure.
+
+```js
+const objobj = require('objobj');
+
+const obj = objobj.set('a.b.c', obj, { bar: 'foo' });
+
+/*
+obj is now:
+{
+    a: {
+        b: {
+            c: 'cheeze',
+        }
+    },
+    bar: 'foo',
+}
+*/
+```
+
+This method take the following arguments:
+
+ * **path** - `String` - The path of the property, in dot notation, to set a value - Required
+ * **value** - `*` - The value to set. Required
+ * **object** - `Object` - A object to append the value too - Optional
+
+When a `path` is defined and it does not pre exist in `object`, a deeply nested
+object structure will be created based on `path`.
+
+If `object` contain parts of the structure of the `path`, this will be left
+untouched and the rest of the structure in `path` will be appended to the
+pre existing structure in `object`.
+
+If a value already exist on the `path`, the value will be overwritten.
+
+If `object` is not provided, a new object will be created.
+
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2018 - Trygve Lie - post@trygve-lie.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- test/objobj.js
+++ test/objobj.js
@@ -3,28 +3,32 @@
 const tap = require('tap');
 const oo = require('../lib/objobj');
 
+/**
+ * .set()
+ */
+
 tap.test('.set() - no arguments - should return empty object', (t) => {
     t.same(oo.set(), {});
     t.end();
 });
 
-tap.test('.set() - shallow path - input: "a", "test", {},  - should return { a: "test" }', (t) => {
+tap.test('.set() - shallow path - input: "a", "test", {} - should return { a: "test" }', (t) => {
     t.same(oo.set('a', 'test', {}), { a: 'test' });
     t.end();
 });
 
-tap.test('.set() - shallow path, override value - input: "a", "test", { a: "foo" },  - should return { a: "test" }', (t) => {
+tap.test('.set() - shallow path, override value - input: "a", "test", { a: "foo" } - should return { a: "test" }', (t) => {
     t.same(oo.set('a', 'test', { a: 'foo' }), { a: 'test' });
     t.end();
 });
 
 
-tap.test('.set() - shallow path, undefined value - input: "a", undefined, {},  - should return { a: undefined }', (t) => {
+tap.test('.set() - shallow path, undefined value - input: "a", undefined, {} - should return { a: undefined }', (t) => {
     t.same(oo.set('a', undefined, {}), { a: undefined });
     t.end();
 });
 
-tap.test('.set() - deep path - input: "a.b.c", "test", {},  - should return { a: { b: { c: "test" } } }', (t) => {
+tap.test('.set() - deep path - input: "a.b.c", "test", {} - should return { a: { b: { c: "test" } } }', (t) => {
     t.same(oo.set('a.b.c', 'test', {}), {
         a: {
             b: {
@@ -35,7 +39,7 @@ tap.test('.set() - deep path - input: "a.b.c", "test", {},  - should return { a:
     t.end();
 });
 
-tap.test('.set() - deep path, partly filled - input: "a.b.c", "test", { a: { bb: "x" } },  - should return { a: { bb: "x", b: { c: "test" } } }', (t) => {
+tap.test('.set() - deep path, partly filled - input: "a.b.c", "test", { a: { bb: "x" } } - should return { a: { bb: "x", b: { c: "test" } } }', (t) => {
     t.same(oo.set('a.b.c', 'test', {
         a: {
             bb: 'x'
@@ -51,7 +55,7 @@ tap.test('.set() - deep path, partly filled - input: "a.b.c", "test", { a: { bb:
     t.end();
 });
 
-tap.test('.set() - deep path, partly filled - input: "a.b.c", "test", { a: { b: "x" } },  - should return { a: { b: { c: "test" } } }', (t) => {
+tap.test('.set() - deep path, partly filled - input: "a.b.c", "test", { a: { b: "x" } } - should return { a: { b: { c: "test" } } }', (t) => {
     t.same(oo.set('a.b.c', 'test', {
         a: {
             b: 'x'
@@ -63,5 +67,50 @@ tap.test('.set() - deep path, partly filled - input: "a.b.c", "test", { a: { b: 
             }
         }
     });
+    t.end();
+});
+
+
+/**
+ * .get()
+ */
+
+tap.test('.get() - no arguments - should return "undefined"', (t) => {
+    t.same(oo.get(), undefined);
+    t.end();
+});
+
+tap.test('.get() - deep path, match deepest property - input: "a.b.c", { a: { b: { c: "test" } } },  - should return "test"', (t) => {
+    t.same(oo.get('a.b.c', {
+        a: {
+            b: {
+                c: 'test'
+            }
+        }
+    }), 'test');
+    t.end();
+});
+
+tap.test('.get() - deep path, match intermediate property - input: "a.b", { a: { b: { c: "test" } } },  - should return { c: "test" }', (t) => {
+    t.same(oo.get('a.b', {
+        a: {
+            b: {
+                c: 'test'
+            }
+        }
+    }), {
+        c: 'test'
+    });
+    t.end();
+});
+
+tap.test('.get() - deep path, no match - input: "x.y", { a: { b: { c: "test" } } },  - should return "undefined"', (t) => {
+    t.same(oo.get('x.y', {
+        a: {
+            b: {
+                c: 'test'
+            }
+        }
+    }), undefined);
     t.end();
 });
